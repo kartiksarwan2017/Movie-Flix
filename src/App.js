fb758753c5a04a4c90ddb1a0a7edbaeac6070a9e
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-10-03 10:30:52 +0530

    Creating the Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import MovieList from "./MovieList";
+import Navbar from "./Navbar";
 
 
 function App() {
   return (
     <>
-     <h1>Movie App</h1>
+     <Navbar />
      <MovieList />
 
     </>
