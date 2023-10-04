367e76a32f3fa2a473901b63acbee2b88f457b75
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-10-04 10:23:57 +0530

    inline styles in React

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,7 +4,12 @@ class Navbar extends React.Component{
     render(){
         return (
             <>
-            <div className="nav">
+            <div style={{ width: "100%", 
+                          height: 70, 
+                          backgroundColor: "red", 
+                          display: "flex", 
+                          justifyContent: "space-between"
+                        }}>
                 <div className="title">Movie-App</div>
                 <div>
                     <img alt="Cart Icon" />
