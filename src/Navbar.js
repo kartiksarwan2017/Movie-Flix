949b345ec3200274cb66c4205733d3ba0d69d11e
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-10-05 10:59:56 +0530

    CSS Scope in React

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,16 +4,11 @@ class Navbar extends React.Component{
     render(){
         return (
             <>
-            <div style={{ width: "100%", 
-                          height: 70, 
-                          backgroundColor: "red", 
-                          display: "flex", 
-                          justifyContent: "space-between"
-                        }}>
-                <div className="title">Movie-App</div>
-                <div>
-                    <img alt="Cart Icon" />
-                    <span>0</span>
+            <div style={styles.nav}>
+                <div style={styles.title}>Movie-App</div>
+                <div style={styles.cartContainer}>
+                    <img src="https://cdn-icons-png.flaticon.com/128/4290/4290854.png" style={styles.cartIcon} alt="Cart Icon" />
+                    <span style={styles.cartCount}>0</span>
                 </div>
             </div>
             </>
@@ -22,3 +17,38 @@ class Navbar extends React.Component{
 }
 
 export default Navbar;
+
+const styles = {
+    nav: {
+       height: 70,
+       background: "#4267b2",
+       display: "flex",
+       justifyContent: "space-between",
+       alignItems: "center",
+       position: "relative"
+    },
+    title: {
+        fontSize: 30,
+        color: "#fff",
+        fontWeight: '"Montserrat", sans-serif',
+        textTransform: "uppercase",
+        marginLeft: 20
+    },
+    cartContainer: {
+        position: "relative",
+        cursor: "pointer"
+    },
+    cartIcon: {
+        height: 48,
+        marginRight: 20
+    },
+    cartCount: {
+        background: "orange",
+        borderRadius: "50%",
+        padding: "4px 8px",
+        position: "absolute",
+        right: 10,
+        top: -5,
+        fontSize: 12
+    }
+}
