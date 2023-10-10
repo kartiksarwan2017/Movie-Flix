0916f74333eb9d0e910666ed312a59ec73be16fc
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-10-10 12:40:33 +0530

    Dynamic styles in Styled Components

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,12 +1,15 @@
 import React from "react";
 import styled from "styled-components";
+const cartIconImg = "https://cdn-icons-png.flaticon.com/128/4290/4290854.png";
 
 const Nav = styled.div`
     width: 100%;
     height: 70px;
-    background-color: blue;
+    background: linear-gradient(170deg, #1bc059, #0d47a1);
     display: flex;
     justify-content: space-between;
+    align-items: center;
+    position: relative;
 `;
 
 const Title = styled.div`
@@ -16,6 +19,29 @@ const Title = styled.div`
     font-family: Montserrat, sans-serif;
     text-transform: uppercase;
     margin-left: 20;
+    &:hover{color: #0f0}
+`;
+
+const CartContainer = styled.div`
+    position: "relative",
+    cursor: "pointer"
+`;
+
+
+const CartIcon = styled.img`
+    height: 48px;
+    margin-right: 20px;
+`;
+
+const CartCount = styled.span`
+    background: ${(props) => props.color};
+    border-radius: 50%;
+    padding: 4px 8px;
+    position: absolute;
+    right: 10px;
+    top: -5px;
+    font-size: 12px;
+    visibility: ${(props) => props.show? "visible": "hidden"}
 `;
 
 class Navbar extends React.Component{
@@ -24,13 +50,13 @@ class Navbar extends React.Component{
             <>
             <Nav>
                 <Title>Movie-App</Title>
-                <div style={styles.cartContainer}>
-                    <img src="https://cdn-icons-png.flaticon.com/128/4290/4290854.png" style={styles.cartIcon} alt="Cart Icon" />
-                    <span style={styles.cartCount}>0</span>
-                </div>
+                <CartContainer> 
+                    <CartIcon src="https://cdn-icons-png.flaticon.com/128/4290/4290854.png" alt="Cart Icon" />
+                    <CartCount color = "yellow" show={true}>0</CartCount>
+                </CartContainer>
             </Nav>
             </>
-        )
+        ) 
     }
 }
 
