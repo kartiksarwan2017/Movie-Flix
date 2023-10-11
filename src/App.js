b5c9882e59f608f0c83c3c7ecdee9fcdcd11d438
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2023-10-11 12:58:22 +0530

    CSS Modules

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import MovieList from "./MovieList";
 import Navbar from "./Navbar";
-
+import HeadingOne from "./HeadingOne";
+import HeadingTwo from "./HeadingTwo";
 
 function App() {
   return (
     <>
-     <Navbar />
-     <MovieList />
+     {/* <Navbar />
+     <MovieList /> */}
+
+     <HeadingOne />
+     <HeadingTwo />
 
     </>
   );
---
+++ src/HeadingTwo.js
@@ -0,0 +1,12 @@
+function HeadingTwo() {
+    return (
+        <>
+         <div className="wrapper">
+            <h1>Heading2</h1>
+            <button className="headbtn">Button2</button>
+         </div>
+        </>
+    )
+}
+
+export default HeadingTwo;
